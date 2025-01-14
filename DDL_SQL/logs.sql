4616cfb63e5e356aee7a4edd18b3cd47da8e8a5f
Henrique Soares Rodrigues <rodriguesoareshenrique@gmail.com>
2025-01-14 14:55:29 -0300

    Update logs.sql

--- DDL_SQL/logs.sql
+++ DDL_SQL/logs.sql
@@ -1,9 +1,11 @@
-CREATE TABLE sistema_predicao_evasao_versao_2.log_predicao (
-  idlog_predicao INT NOT NULL AUTO_INCREMENT,
-  data DATETIME NULL,
-  predicao VARCHAR(60) NULL,
-  curso VARCHAR(60) NULL,
-  matricula VARCHAR(60) NULL,
-  PRIMARY KEY (idlog_predicao)
-);
+CREATE TABLE `log_predicao` (
+  `idlog_predicao` int NOT NULL AUTO_INCREMENT,
+  `data` datetime DEFAULT NULL,
+  `predicao` varchar(60) DEFAULT NULL,
+  `curso` varchar(60) DEFAULT NULL,
+  `usuario` varchar(200) DEFAULT NULL,
+  `ip_usuario` varchar(45) DEFAULT NULL,
+  `matricula` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`idlog_predicao`)
+)
 
